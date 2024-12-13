bd0ff46f047ec84978916193247d6f039da59f6a
musti <musti@duck.com>
2024-12-13 21:50:12 +0300

    bump to 0.9.3

--- cliwrap.rb
+++ cliwrap.rb
@@ -1,8 +1,8 @@
 class Cliwrap < Formula
     desc "Spotify Wrapped-like statistics for your terminal usage"
     homepage "https://github.com/islemci/cliwrap"
-    url "https://github.com/islemci/cliwrap/archive/refs/tags/0.9.2.tar.gz"
-    sha256 "c89d68fbae74567170d07efe5545cc95f0ea485e34eebf8521ef51a1cf0baba7"
+    url "https://github.com/islemci/cliwrap/archive/refs/tags/0.9.3.tar.gz"
+    sha256 "10acd122c09e7cf923d9f5d321835cd5c48dcd3793791208c1a36dae9f48b5cb"
     license "MIT"
   
     def install
